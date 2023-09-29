da1464788025d186578b5275af9030e0d9d1e114
IslomSFD <islom.developerfd@gmail.com>
2023-09-29 19:29:05 +0500

    commmit l

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,6 @@ export default {
           <div class="img_box">
             <img :src="data?.image" alt="Image">
           </div>
-          s
           <h3 class="product_title">{{ data?.title }}</h3>
           <button @click="handleBasket(data)" class="product_btn">Qo'shish</button>
         </div>
